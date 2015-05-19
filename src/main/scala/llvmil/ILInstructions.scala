c35032cdfe01c3819e744434f3f5bf99676398c3
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-20 01:33:32 +0200

    move Chains to bottom

--- src/main/scala/llvmil/ILInstructions.scala
+++ src/main/scala/llvmil/ILInstructions.scala
@@ -17,6 +17,43 @@ object ILInstructions {
   case class Br(dest: String) extends ILInstruction
 
   sealed class ILOperation(val retType: Type)
+  // Values identified by Strings of some sort.
+  sealed class Identifier(tpe: Type, val name: String) extends ILOperation(tpe)
+  case class Local(tpe: Type, nme: String) extends Identifier(tpe, '%' + nme)
+  case class Global(tpe: Type, nme: String) extends Identifier(tpe, '@' + nme)
+  case class IConst(tpe: TInteger, i: Int) extends Identifier(tpe, i.toString)
+  case class Bitcast(to: Type, id: Identifier) extends Identifier(to, id.name)
+
+  object Const {
+    def apply(s: String)(implicit mtd: Method) = mtd.scp(s)
+    def apply(i: Int) = IConst(TInt, i)
+  }
+
+  // Arithmetics
+  case class Add(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
+  case class Sub(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
+  case class Mul(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
+  case class UDiv(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
+  case class SDiv(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
+
+  // Memory
+  case class Alloca(tpe: Type) extends ILOperation(TPointer(tpe))
+  case class Load(ptr: Identifier) extends ILOperation({
+    val TPointer(inner) = ptr.retType
+    inner
+  })
+
+  //TODO(mrksr): Implement the Type derivation
+  // case class GetElementPtr(ptr: Identifier, idxs: List[Int]) extends ILOperation(TVoid)
+
+  // OOP
+  case class VirtualResolve(obj: Identifier, name: String, args: List[Type], retTpe: Type)
+    extends ILOperation(TFunction(args, retTpe))
+  case class AccessField(obj: Identifier, name: String, tpe: Type) extends ILOperation(tpe)
+  case class Call(func: Identifier, args: List[Identifier]) extends ILOperation({
+    val TFunction(_, ret) = func.retType
+    ret
+  })
 
   type ILOperationPipeline = (Method => Option[Identifier])
   case class ILOperationChain private[ILInstructions](pipe: ILOperationPipeline) {
@@ -60,42 +97,4 @@ object ILInstructions {
       Some(id)
     }))
   implicit def chainToPipeline(pipe: ILOperationChain): ILOperationPipeline = pipe.pipe
-
-  object Const {
-    def apply(s: String)(implicit mtd: Method) = mtd.scp(s)
-    def apply(i: Int) = IConst(TInt, i)
-  }
-
-  // Values identified by Strings of some sort.
-  sealed class Identifier(tpe: Type, val name: String) extends ILOperation(tpe)
-  case class Local(tpe: Type, nme: String) extends Identifier(tpe, '%' + nme)
-  case class Global(tpe: Type, nme: String) extends Identifier(tpe, '@' + nme)
-  case class IConst(tpe: TInteger, i: Int) extends Identifier(tpe, i.toString)
-  case class Bitcast(to: Type, id: Identifier) extends Identifier(to, id.name)
-
-  // Arithmetics
-  case class Add(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
-  case class Sub(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
-  case class Mul(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
-  case class UDiv(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
-  case class SDiv(lhs: Identifier, rhs: Identifier) extends ILOperation(lhs.retType)
-
-  // Memory
-  case class Alloca(tpe: Type) extends ILOperation(TPointer(tpe))
-  case class Load(ptr: Identifier) extends ILOperation({
-    val TPointer(inner) = ptr.retType
-    inner
-  })
-
-  //TODO(mrksr): Implement the Type derivation
-  // case class GetElementPtr(ptr: Identifier, idxs: List[Int]) extends ILOperation(TVoid)
-
-  // OOP
-  case class VirtualResolve(obj: Identifier, name: String, args: List[Type], retTpe: Type)
-    extends ILOperation(TFunction(args, retTpe))
-  case class AccessField(obj: Identifier, name: String, tpe: Type) extends ILOperation(tpe)
-  case class Call(func: Identifier, args: List[Identifier]) extends ILOperation({
-    val TFunction(_, ret) = func.retType
-    ret
-  })
 }
