e073518e340a9701e70e2af938c8fce782cd7daa
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-21 15:12:21 +0200

    oops

--- src/main/scala/llvmil/Types.scala
+++ src/main/scala/llvmil/Types.scala
@@ -14,7 +14,7 @@ object Types {
     def toIL() = "i%d".format(size)
   }
   case class TStruct(name: String, fields: List[Type]) extends Type {
-    def toIL() = "\%%s%s".format(Prefixes.struct, name)
+    def toIL() = "%%%s%s".format(Prefixes.struct, name)
     def declaration() = "%s = { %s }".format(toIL(), fields.map(_.toIL()).mkString(", "))
   }
   case class TFunction(args: List[Type], retTpe: Type) extends Type {
@@ -35,5 +35,5 @@ object Types {
   val TString = TStruct("String", List(TInt, TArray(0, TInteger(8))))
   val TIntArray = TStruct("IntArray", List(TInt, TArray(0, TInt)))
 
-  def TVTable(size: Int) = TArray(size, TPointer(TFunction(List(TVariadic), TVoid))
+  def TVTable(size: Int) = TArray(size, TPointer(TFunction(List(TVariadic), TVoid)))
 }
