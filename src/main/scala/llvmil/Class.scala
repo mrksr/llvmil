685b0e3b474d20d1dfc25351101a73fa18d31e94
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-21 21:27:12 +0200

    reorder

--- src/main/scala/llvmil/Class.scala
+++ src/main/scala/llvmil/Class.scala
@@ -8,6 +8,26 @@ class Class private[llvmil]( val className: String,
                              prog: Program
                            ) {
   private type MethodInfo = (String, List[Type], Type)
+
+  var fields: List[(Type, String)] = Nil
+  var methods: List[(Function, Option[MethodInfo])] = Nil
+
+  def addField(name: String, tpe: Type): Unit = {
+    fields = fields ::: ((tpe, name) :: Nil)
+  }
+
+  def addMethod( name: String,
+                 args: List[(Type, String)],
+                 retTpe: Type,
+                 overrides: Option[MethodInfo]
+               ): Function = {
+    val mtd = new Function(name, args, retTpe, prog.sp)
+    methods = methods ::: ((mtd, overrides) :: Nil)
+
+    mtd
+  }
+
+  // The Lazys MUST be evaluated after all classes exist!
   lazy val vTable: List[Function] = {
     val parentMethods = parentName.map(prog.classes).map(_.vTable).getOrElse(Nil)
     parentMethods ::: methods.filter(_._2.isEmpty).map(_._1)
@@ -28,23 +48,5 @@ class Class private[llvmil]( val className: String,
       "%s%s".format(Prefixes.classType, className),
       vTableField :: allFieldTypes
     )
-  }
-
-  var fields: List[(Type, String)] = Nil
-  var methods: List[(Function, Option[MethodInfo])] = Nil
-
-  def addField(name: String, tpe: Type): Unit = {
-    fields = fields ::: ((tpe, name) :: Nil)
-  }
-
-  def addMethod( name: String,
-                 args: List[(Type, String)],
-                 retTpe: Type,
-                 overrides: Option[MethodInfo]
-               ): Function = {
-    val mtd = new Function(name, args, retTpe, prog.sp)
-    methods = methods ::: ((mtd, overrides) :: Nil)
-
-    mtd
   }
 }
