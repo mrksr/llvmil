f06085c92a6c73da0bf8433d54d0cf6d196a764b
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-21 22:06:05 +0200

    remove cluttering parens

--- src/main/scala/llvmil/Types.scala
+++ src/main/scala/llvmil/Types.scala
@@ -4,30 +4,30 @@ import Prefixes._
 
 object Types {
   sealed trait Type {
-    def toIL(): String
+    def toIL: String
   }
 
   case object TVoid extends Type {
-    def toIL() = "void"
+    def toIL = "void"
   }
   case class TInteger(size: Int) extends Type {
-    def toIL() = "i%d".format(size)
+    def toIL = "i%d".format(size)
   }
   case class TStruct(name: String, fields: List[Type]) extends Type {
-    def toIL() = "%%%s%s".format(Prefixes.struct, name)
-    def declaration() = "%s = { %s }".format(toIL(), fields.map(_.toIL()).mkString(", "))
+    def toIL = "%%%s%s".format(Prefixes.struct, name)
+    def declaration() = "%s = { %s }".format(toIL, fields.map(_.toIL).mkString(", "))
   }
   case class TFunction(args: List[Type], retTpe: Type) extends Type {
-    def toIL() = "%s (%s)".format(retTpe.toIL(), args.map(_.toIL()).mkString(", "))
+    def toIL = "%s (%s)".format(retTpe.toIL, args.map(_.toIL).mkString(", "))
   }
   case class TArray(length: Int, tpe: Type) extends Type {
-    def toIL() = "[%d x %s]".format(length, tpe.toIL())
+    def toIL = "[%d x %s]".format(length, tpe.toIL)
   }
   case class TPointer(target: Type) extends Type {
-    def toIL() = "%s *".format(target.toIL())
+    def toIL = "%s *".format(target.toIL)
   }
   case object TVariadic extends Type {
-    def toIL() = "..."
+    def toIL = "..."
   }
 
   val TBool = TInteger(1)
