a2f99668b946493722b613dc06a524eb0956ee0a
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-21 21:06:20 +0200

    static functions

--- src/main/scala/llvmil/Program.scala
+++ src/main/scala/llvmil/Program.scala
@@ -1,16 +1,29 @@
 package llvmil
 
+import Types._
+
 class Program extends ConstantPool {
   private[llvmil] val sp: StringPool = new StringPool()
   def string(const: String) = sp.string(const)
 
   private[llvmil] var classes: Map[String, Class] = Map()
+  private[llvmil] var statics: List[Function] = Nil
 
   def addClass(className: String, parentName: Option[String]): Class = {
     val cls = new Class(className, parentName, this)
     classes += (className -> cls)
 
     cls
+  }
+
+  def addStatic( name: String,
+                 args: List[(Type, String)],
+                 retTpe: Type
+               ): Function = {
+    val fnc = new Function(name, args, retTpe, sp)
+    statics = statics ::: (fnc :: Nil)
+
+    fnc
   }
 
   def writeToFile(fileName: String) = {
